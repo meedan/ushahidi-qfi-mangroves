2d70f6416a6dc8b37f48182d0fd555fc8d44bead
Chris Blow <unthinkingly@gmail.com>
2012-10-26 12:10:51 -1000

    fix logo with hardcoded theme image

--- themes/mangroves/views/header.php
+++ themes/mangroves/views/header.php
@@ -57,16 +57,10 @@
 		<!-- header -->
 		<div id="header">
 
-			<!-- logo -->
-			<?php if($banner == NULL){ ?>
-			<div id="logo">
-				<h1><a href="<?php echo url::site();?>"><?php echo $site_name; ?></a></h1>
-				<span><?php echo $site_tagline; ?></span>
-			</div>
-			<?php }else{ ?>
-			<a href="<?php echo url::site();?>"><img src="<?php echo $banner; ?>" alt="<?php echo $site_name; ?>" /></a>
-			<?php } ?>
-			<!-- / logo -->
+
+			<a href="<?php echo url::site();?>">
+				<img src="/themes/mangroves/images/logo.png" alt="<?php echo $site_name; ?>" />
+			</a>
 
 			<!-- submit incident -->
 			<?php echo $submit_btn; ?>
